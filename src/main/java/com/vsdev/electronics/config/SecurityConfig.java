af34bec3b70a2829c0f407b396aa09ffea984ff5
VitalieVS <vitaliesandiuc@gmail.com>
2021-08-22 11:40:17 +0300

    SecurityConfig.java done

--- src/main/java/com/vsdev/electronics/config/SecurityConfig.java
+++ src/main/java/com/vsdev/electronics/config/SecurityConfig.java
@@ -1,6 +1,68 @@
 package com.vsdev.electronics.config;
 
+import com.vsdev.electronics.entity.Permission;
+import com.vsdev.electronics.entity.Role;
+import com.vsdev.electronics.repository.RoleRepository;
+import com.vsdev.electronics.service.UserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        List<Role> roles = roleRepository.findAll();
+        http.csrf().disable();
+
+        for (Role role : roles) {
+            for (Permission permission : role.getPermissions()) {
+                http.authorizeRequests()
+                        .antMatchers(permission.getName()).hasAuthority(permission.getName());
+            }
+        }
+
+        http.authorizeRequests()
+                .antMatchers("/auth").permitAll().anyRequest()
+                .authenticated().and().exceptionHandling()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 }
--- src/main/java/com/vsdev/electronics/service/UserDetailsService.java
+++ src/main/java/com/vsdev/electronics/service/UserDetailsService.java
@@ -5,14 +5,18 @@ import com.vsdev.electronics.entity.Role;
 import com.vsdev.electronics.entity.User;
 import com.vsdev.electronics.repository.RoleRepository;
 import com.vsdev.electronics.repository.UserRepository;
+import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.*;
 
+@Service
+@Builder
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
 
     @Autowired
