7c0acb5147eb71c52ac63222e0b4d8d52b53bfec
VitalieVS <vitaliesandiuc@gmail.com>
2021-09-12 14:12:30 +0300

    Uploaded pics

--- src/main/resources/db/migration/V16__CREATE__POPULATE__PROMOTIONS.sql
+++ src/main/resources/db/migration/V16__CREATE__POPULATE__PROMOTIONS.sql
@@ -11,11 +11,11 @@ CREATE TABLE promotions
 
 INSERT INTO promotions(id, title, body, image, food_type, price)
 VALUES (1, 'New Burgers', 'Try out our new delicious burgers! Buy 2 and receive 3 free',
-        'http://10.0.2.2:4546/images/pankace.jpg', 'Non vegetarian - Eggs', 85);
+        'http://10.0.2.2:4546/images/burgers.jpg', 'Non vegetarian - Eggs', 85);
 
 INSERT INTO promotions(id, title, body, image, food_type, price)
 VALUES (2, 'Pizza 4 cheeses', 'Try out our new pizza! Buy 2 and receive 3 free',
-        'http://10.0.2.2:4546/images/pankace.jpg', 'Vegetarian pizza cheese', 125);
+        'http://10.0.2.2:4546/images/4cheese.jpg', 'Vegetarian pizza cheese', 125);
 
 INSERT INTO promotions(id, title, body, image, food_type, price)
 VALUES (3, 'Black Forest Pancakes', 'Try out our new delicious Black Forest Pancakes!',
